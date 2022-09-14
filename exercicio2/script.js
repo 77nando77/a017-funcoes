70d18f1618c1a04e0f5aee307da83570f60acfab
luizfk <luiz.kessler7@gmail.com>
2022-09-14 14:03:30 -0300

    funcoes

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -8,33 +8,33 @@
 
 // d) Uma função que recebe uma mensagem (`string`) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela contendo apenas letras maiúsculas.
 
-// const soma = (num1,num2) =>{
-//     resp = num1 + num2
-//     console.log(`${num1} + ${num2} = ${resp}`)
-//     return resp
-// }
-// console.log(soma(Number(prompt("Digite um número que deseja somar")),Number(prompt("Digite outro número que desejar somar"))))
+const soma = (num1,num2) =>{
+    resp = num1 + num2
+    console.log(`${num1} + ${num2} = ${resp}`)
+    return resp
+}
+console.log(soma(Number(prompt("Digite um número que deseja somar")),Number(prompt("Digite outro número que desejar somar"))))
 
-// const vOuF =(num1,num2) => {
-//     console.log(`Primeiro número é ${num1}`);
-//     console.log(`Segundo número é ${num2}`);
-//     resp = num1 >= num2
-//     return resp
-// }
-// console.log(`Primeiro número é maior e diferente que o segundo?: ${vOuF(Number(prompt("Digite um número")),Number(prompt("Digite outro número")))}`);
+const vOuF =(num1,num2) => {
+    console.log(`Primeiro número é ${num1}`);
+    console.log(`Segundo número é ${num2}`);
+    resp = num1 >= num2
+    return resp
+}
+console.log(`Primeiro número é maior e diferente que o segundo?: ${vOuF(Number(prompt("Digite um número")),Number(prompt("Digite outro número")))}`);
 
-// const par = (num) =>{
-//     if (num%2 ===0){
-//         return "Esse número é par"
-//     }else{
-//         return "Esse número não é par"
-//     }
-// }
-// let numero = Number(prompt("Digite qualquer número"))
-// console.log(`Você digitou ${numero}: ${par(numero)}`)
+const par = (num) =>{
+    if (num%2 ===0){
+        return "Esse número é par"
+    }else{
+        return "Esse número não é par"
+    }
+}
+let numero = Number(prompt("Digite qualquer número"))
+console.log(`Você digitou ${numero}: ${par(numero)}`)
 
 const mensagem = (mensa) => {
-    console.log (`${mensa.trim()} tem ${mensa.trim().length} letras
+    console.log (`${mensa.trim()} tem ${mensa.replace(" ","").trim().length} letras
 e em letras maiúsculas fica ${mensa.toUpperCase().trim()}`)
 }
 
